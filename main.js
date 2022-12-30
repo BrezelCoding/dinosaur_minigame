8e3334b3a8cb4b8eb47329528a8bce7e295b4447
Hyeon Kim <stuttgart.kim@gmail.com>
2022-12-30 23:42:11 +0100

    JS: random cactus

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 
 const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
+const CANVAS_HEIGHT = 400;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-let timer = 0;
+let timer;
 let cactusArray = [];
 let jumping = false;
 let jumpTimer = 0;
@@ -24,42 +24,45 @@ cactusImg.src = 'cactus.png';
 const dino = {
   x: 10,
   y: 10,
-  width: 10,
-  height: 10,
+  width: 150,
+  height: 150,
   draw() {
     // ctx.fillStyle = 'green';
     // ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(dinoImg, this.x, this.y);
+    ctx.drawImage(dinoImg, this.x, this.y, this.width, this.height);
   },
 };
 
 class Cactus {
   constructor() {
-    this.x = 500;
-    this.y = 200;
-    this.width = 50;
-    this.height = 50;
+    this.x = 720;
+    this.y = 310;
+    this.width = 70;
+    this.height = 90;
   }
   draw() {
     // ctx.fillStyle = 'red';
     // ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(cactusImg, this.x, this.y);
+    ctx.drawImage(cactusImg, this.x, this.y, this.width, this.height);
   }
 }
 
 /* 1초에 60번 코드 실행하기 */
 function gameExcute() {
   animation = requestAnimationFrame(gameExcute);
-  timer++;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 애니메이션 잔상 없애주는 clearRect
 
-  if (timer % 120 === 0) {
+  let randomTime = Math.floor(Math.random() * 2000);
+  console.log(randomTime);
+
+  if (randomTime % 100 === 0 && randomTime < 500) {
     const cactus = new Cactus();
     cactusArray.push(cactus);
   }
+
   cactusArray.forEach((cactus, index, array) => {
-    if (cactus.x < 0) {
+    if (cactus.x < -70) {
       // x 좌표가 0 미만이면 제거
       array.splice(index, 1);
     }
@@ -77,7 +80,7 @@ function gameExcute() {
     jumpTimer = 0;
   }
   if (jumping == false) {
-    if (dino.y < 200) {
+    if (dino.y < 250) {
       dino.y += 3;
     }
   }
@@ -89,8 +92,8 @@ gameExcute();
 
 // 충돌감지 collision detection
 function collision(dino, cactus) {
-  let xGap = cactus.x - (dino.x + dino.width);
-  let yGap = cactus.y - (dino.y + dino.height);
+  let xGap = cactus.x - (dino.x + dino.width) + 10;
+  let yGap = cactus.y - (dino.y + dino.height) + 27;
   if (xGap < 0 && yGap < 0) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     cancelAnimationFrame(animation);
